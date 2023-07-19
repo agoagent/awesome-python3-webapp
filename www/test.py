11e4c735766dea059115610b57a4e5150b257e73
agoagent <838823042@qq.com>
2023-07-19 19:38:42 +0800

    add orm and test code

---
+++ www/test.py
@@ -0,0 +1,4 @@
+t = lambda f: '`%s`' % f
+m = map(lambda f: '`%s`' % f, [1,'2', True])
+for m2 in m:
+    print(m2)
---
+++ www/test_orm.py
@@ -0,0 +1,12 @@
+import asyncio
+import orm
+from models import User
+async def test(loop):
+    await orm.create_pool(user='root', password='root', db='awesome', loop=loop)
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='abut:blank')
+    await u.save()
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(test(loop))
